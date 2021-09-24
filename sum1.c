d057da18bb551790da2d07eb25e24464b5047989
Abhay Saini <90948162+abhaycodee@users.noreply.github.com>
2021-09-24 17:45:37 +0530

    Add files via upload

---
+++ sum1.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int main(){
+	
+int sum=0,n,i;
+printf("enter a number n");
+scanf("%d",&n);
+	
+for(i=1;i<=n;i++){sum =sum+ i;
+}	
+printf("sum of %d number is= %d",n,sum);
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
